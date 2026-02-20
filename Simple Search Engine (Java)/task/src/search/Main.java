3d86064f89d51225f257bbf4e011a2ccaa18acf8
adantuluri-grid <adantuluri@griddynamics.com>
2026-02-20 11:43:36 +0530

    Hyperskill

---
+++ Simple Search Engine (Java)/task/src/search/Main.java
@@ -0,0 +1,28 @@
+package search;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] words = sc.nextLine().split(" ");
+
+        String target = sc.nextLine();
+
+        int index = -1;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
+                index = i + 1;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println(index);
+        } else {
+            System.out.println("Not Found");
+        }
+    }
+}
